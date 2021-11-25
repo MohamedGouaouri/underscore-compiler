ca025a33be326455904abb78c9b5bc0326db31f6
Benelhadj Dj. Mama Saadia <51034305+SouaadMa@users.noreply.github.com>
2021-11-26 00:00:44 +0100

    Added scanner.l and scanner.h

---
+++ scanner.h
@@ -0,0 +1,68 @@
+
+#define _LOOP 0
+#define _READ 0
+#define _WRITE 0
+
+#define _IF 0
+#define _ELSE 0
+
+#define _ENTRY 0
+#define _DOT 0
+#define _COMMA 0
+#define _SEMICOLON 0
+
+#define _FUNCTIONDECLARE 0
+#define _NUMBERDECLARE 0
+#define _STRINGDECLARE 0
+#define _CONSTDECLARE 0
+#define _BOOLEENDECLARE 0
+#define _STRUCTTYPEDECLARE 0
+#define _STRUCTDECLARE 0
+#define _POINTERDECLARE 0
+#define _TABLEDECLARE 0
+
+#define _ADDRESSVALUE 0
+#define _POINTERVALUE 0
+
+#define _ASSIGNMENT 0
+#define _RETURN 0
+
+#define _INTEGER 0
+#define _REALNUMBER 0
+#define _STRING 0
+
+#define _OPENPARENTHESIS 0
+#define _CLOSEPARENTHESIS 0
+#define _OPENHOOK 0
+#define _CLOSEHOOK 0
+#define _OPENBRACKET 0
+#define _CLOSEBRACKET 0
+
+#define _ID 0
+
+#define _EQUAL 0
+#define _NONEQUAL 0
+#define _AND 0
+#define _OR 0
+#define _NON 0
+#define _INFERIOR 0
+#define _SUPERIOR 0
+#define _INFERIOREQUAL 0
+#define _SUPERIOREQUAL 0
+#define _ADD 0
+#define _SUB 0
+#define _MULT 0
+#define _DIV 0
+#define _MOD 0
+#define _POWER 0
+
+
+
+
+
+
+
+#define _EOF 0
+#define _ERRO <number here>
+#define _SEMICOLO <number here>
+#define _EQ <number here>
